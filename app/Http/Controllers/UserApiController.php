84ed37ab17c6db1b27be04392306691e4815826e
poori-m <mpouya050@gmail.com>
2022-11-23 20:20:56 +0330

    RESTfulApi - CRUD database

---
+++ app/Http/Controllers/UserApiController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class UserApiController extends Controller
+{
+    //index => show users
+    public function index()
+    {
+        return User::all();
+    }
+
+    //for create user by method post
+    public function store()
+    {
+        return User::create([
+                'name' => request('name'),
+                'family' => request('family'),
+                'email' => request('email'),
+                'phone_number' => request('phone_number'),
+                'password' => Hash::make(request('password'))
+            ]
+        );
+    }
+
+    //edit User
+    public function update(User $user)
+    {
+        $user->update([
+            'name' => request('name'),
+            'family' => request('family'),
+            'email' => request('email'),
+            'phone_number' => request('phone_number'),
+        ]);
+
+        return $user;
+    }
+
+//delete user
+    public function destroy(User $user)
+    {
+        $success = $user->delete();
+        return [
+            'success' => $success
+        ];
+    }
+}
